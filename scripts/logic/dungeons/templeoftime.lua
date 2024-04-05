2932ecf8f99ac6629c63b72d53d83c0c53e9f7eb
Phantom <phantom@phantom-games.com>
2024-04-05 01:41:27 -0700

    rename dominion rod 1

--- scripts/logic/dungeons/templeoftime.lua
+++ scripts/logic/dungeons/templeoftime.lua
@@ -2,17 +2,17 @@
   Dungeon Progress
 ---------------------------------------------------------]]
 function canAccessToT()
-  return canCompletePrologue() 
-          and has("Shadow_Crystal") 
+  return canCompletePrologue()
+          and has("Shadow_Crystal")
           and (canDefeatShadowBeast() and has("Progressive_Sword_3") or has("early-tot-open"))
 end
 
 function canCompleteTempleofTime()
-  return canAccessToT() 
-          and has("Progressive_Dominion_Rod_1") 
-          and has("Progressive_Bow") 
-          and has("Spinner") 
-          and (has("Temple_of_Time_Small_Key",3) or has("small-keysy-enabled"))
+  return canAccessToT()
+          and has("Progressive_Dominion_Rod")
+          and has("Progressive_Bow")
+          and has("Spinner")
+          --and (has("Temple_of_Time_Small_Key",3) or has("small-keysy-enabled")) -- never need small keys if first chest has boss key
           and (has("Temple_of_Time_Big_Key") or has("boss-keysy-enabled"))
           and canDefeatArmogohma()
 end
@@ -26,9 +26,22 @@ end
 
 function canDefeatArmogohma()
   return has("Progressive_Bow")
-          and has("Progressive_Dominion_Rod_1")
+          and has("Progressive_Dominion_Rod")
 end
 
 --[[-------------------------------------------------------
   Individual Checks
 ---------------------------------------------------------]]
+function Temple_of_Time_2F_Access()
+  local key = has("Temple_of_Time_Small_Key",1) or has("small-keysy-enabled")
+  return key
+end
+
+function Temple_of_Time_Dungeon_Reward()
+  local key = has("Temple_of_Time_Big_Key") or has("boss-keysy-enabled")
+  return key and has("Progressive_Bow") and has("Progressive_Dominion_Rod")
+end
+
+function Temple_of_Time_Lobby_Lantern_Chest()
+  return has("Lantern")
+end
