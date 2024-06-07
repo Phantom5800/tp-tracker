a45a28d94d83645daf207c46599a063621fb21a1
Phantom <phantom@phantom-games.com>
2024-06-06 20:48:18 -0700

    Small key count fixes for Lakebed, overall logic fixes should start going in sooner rather than later

--- scripts/logic/dungeons/lakebed.lua
+++ scripts/logic/dungeons/lakebed.lua
@@ -104,6 +104,10 @@ function Lakebed_East_Lower_Waterwheel_Stalactite_Chest()
   return canLaunchBombs()
 end
 
+function Lakebed_East_Second_Floor_Chests()
+  return (has("Lakebed_Temple_Small_Key", 1) or has("small-keysy-enabled"))
+end
+
 function Lakebed_East_Water_Supply_Clawshot_Chest()
   return has("Clawshot")
 end
@@ -113,7 +117,7 @@ function Lakebed_Stalactite_Room_Small_Chest()
 end
 
 function Lakebed_Water_Supply_Rooms()
-  return (has("Lakebed_Temple_Small_Key", 3) or has("small-keysy-enabled"))
+  return (has("Lakebed_Temple_Small_Key", 2) or has("small-keysy-enabled"))
 end
 
 function Lakebed_West_Lower_Small_Chest()
